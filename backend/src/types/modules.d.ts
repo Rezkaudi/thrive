914e20f79cee8e6e76a8d21e4743618a6c08e537
Rezkaudi <audirezk@gmail.com>
2025-07-05 11:48:08 +0300

    deplo2

---
+++ backend/src/types/modules.d.ts
@@ -0,0 +1,7 @@
+declare module 'express';
+declare module 'jsonwebtoken';
+declare module 'nodemailer';
+declare module 'multer';
+declare module 'cors';
+declare module 'morgan';
+declare module 'compression';
