f2bbaaea31cb1e63ab8846e01a614a6f80a85989
Rezkaudi <audirezk@gmail.com>
2025-07-12 12:54:10 +0300

    fix PaymentService api version

--- backend/src/infrastructure/services/PaymentService.ts
+++ backend/src/infrastructure/services/PaymentService.ts
@@ -6,7 +6,7 @@ export class PaymentService implements IPaymentService {
 
   constructor() {
     this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-      apiVersion: '2025-05-28.basil'
+      apiVersion: '2025-06-30.basil'
     });
   }
 
