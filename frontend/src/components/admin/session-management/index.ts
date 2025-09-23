985390a003f2ffa277f3835ba54c65afb1853c2e
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2025-09-23 18:35:39 +0300

    clean up session management page

---
+++ frontend/src/components/admin/session-management/index.ts
@@ -0,0 +1,5 @@
+export { SessionManagementHeader } from './SessionManagementHeader';
+export { StatsCards } from './StatsCards';
+export {Filters} from './Filters'
+export {SessionsTable} from './SessionsTable'
+export {DeleteSessionDialog} from './DeleteSessionDialog'
