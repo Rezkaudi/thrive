4ce21253852b874cabbffc6e8cb996d62b2a3d2d
Hakam aldeen Alkhadraa <hakamha8@gmail.com>
2026-03-05 00:04:53 +0300

    feat: add AdminNavigationButton component to various admin pages for improved navigation

--- frontend/src/pages/admin/AdminVideoPage.tsx
+++ frontend/src/pages/admin/AdminVideoPage.tsx
@@ -1,9 +1,8 @@
 // frontend/src/pages/admin/AdminVideoPage.tsx
 import React from 'react';
-import { Container, Typography, Box, Breadcrumbs, Link } from '@mui/material';
-import { Home, AdminPanelSettings } from '@mui/icons-material';
-import { Link as RouterLink } from 'react-router-dom';
+import { Container } from '@mui/material';
 import { VideoManagement } from '../../components/admin/VideoManagement';
+import AdminNavigationButton from '../../components/admin/AdminNavigationButton';
 
 export const AdminVideoPage: React.FC = () => {
   return (
